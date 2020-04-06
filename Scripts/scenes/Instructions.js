6c666aa552fe8b285951ee5a6ce863c30bfd1ab5
dl-ac <44243088+dl-ac@users.noreply.github.com>
2020-04-06 11:38:01 -0400

    Instructions screem

--- Scripts/scenes/Instructions.js
+++ Scripts/scenes/Instructions.js
@@ -26,8 +26,22 @@ var scenes;
         // PRIVATE METHODS
         // PUBLIC METHODS
         Instructions.prototype.Start = function () {
+            var rulesText = "Moviments: " +
+                "\n- Keys W A S D - Player Movement" +
+                "\n- Keys Up Left Down Right - Player Movement" +
+                "\n- Spacebar - Player shoots" +
+                "\n\nRules:" +
+                "\n- Player being hit by an enemy  or bullet, it will lose one life" +
+                "\n- Player loses the game, if it runs out of lives" +
+                "\n- Player wins the game, if it kill the boss" +
+                "\n\nScore:" +
+                "\n- Killing enemies grants 10 points" +
+                "\n- Collecting crytals grants 50 points" +
+                "\n- Killing the boss grants 500 points";
             //instantiate a new Text object
-            this._windowTitle = new objects.Label("Instructions", "36px", "EthnocentricReg", config.Game.TEXT_COLOR, 328, 10, false);
+            this._windowTitle = new objects.Label("Instructions", "36px", "EthnocentricReg", config.Game.TEXT_COLOR, config.Game.SCREEN_WIDTH * 0.5, // 328 x 10
+            50, true);
+            this._rules = new objects.Label(rulesText, "18px", "EthnocentricReg", config.Game.TEXT_COLOR, 70, 100, false);
             // buttons
             this._menuButton = new objects.Button("buttonMenu", 512, 536, true);
             // Background
@@ -40,6 +54,7 @@ var scenes;
         Instructions.prototype.Main = function () {
             this.addChild(this._background);
             this.addChild(this._windowTitle);
+            this.addChild(this._rules);
             this.addChild(this._menuButton);
             this._menuButton.on("click", function () {
                 config.Game.SCENE_STATE = scenes.State.START;
--- Scripts/scenes/Instructions.ts
+++ Scripts/scenes/Instructions.ts
@@ -2,6 +2,7 @@ module scenes {
     export class Instructions extends objects.Scene {
         // PRIVATE INSTANCE MEMBERS
         private _windowTitle: objects.Label;
+        private _rules: objects.Label;
         private _menuButton: objects.Button;
         private _background: objects.Background;
 
@@ -18,14 +19,38 @@ module scenes {
 
         // PUBLIC METHODS
         public Start(): void {
+            let rulesText =
+                "Moviments: " +
+                "\n- Keys W A S D - Player Movement" +
+                "\n- Keys Up Left Down Right - Player Movement" +
+                "\n- Spacebar - Player shoots" +
+                "\n\nRules:" +
+                "\n- Player being hit by an enemy  or bullet, it will lose one life" +
+                "\n- Player loses the game, if it runs out of lives" +
+                "\n- Player wins the game, if it kill the boss" +
+                "\n\nScore:" +
+                "\n- Killing enemies grants 10 points" +
+                "\n- Collecting crytals grants 50 points" +
+                "\n- Killing the boss grants 500 points";
+
             //instantiate a new Text object
             this._windowTitle = new objects.Label(
                 "Instructions",
                 "36px",
                 "EthnocentricReg",
                 config.Game.TEXT_COLOR,
-                328,
-                10,
+                config.Game.SCREEN_WIDTH * 0.5, // 328 x 10
+                50,
+                true
+            );
+
+            this._rules = new objects.Label(
+                rulesText,
+                "18px",
+                "EthnocentricReg",
+                config.Game.TEXT_COLOR,
+                70,
+                100,
                 false
             );
 
@@ -45,6 +70,8 @@ module scenes {
             this.addChild(this._background);
 
             this.addChild(this._windowTitle);
+
+            this.addChild(this._rules);
 
             this.addChild(this._menuButton);
 
